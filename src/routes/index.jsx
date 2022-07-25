4dc074c8bcd68ee488ee71fc5d6f4d6e61469839
Nguyên <nguyentrandn33@gmail.com>
2022-07-25 17:12:33 +0700

    hoi hoi

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,7 @@ import Videos from "../pages/Videos"
 
 //
 const publicRoutes =[ 
-    { path: '/', component: Home},
+    { path: '/repoSearch', component: Home},
     { path: '/news', component: News},
     { path: '/videos', component: Videos},
 
